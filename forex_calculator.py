073efb6fb4e7ae39698d048715cd5ee323f2ea2b
Alex Baracat <35846995+alexbaracat@users.noreply.github.com>
2023-04-05 00:30:52 -0400

    Update forex_calculator.py
    Added risk calculator.

--- forex_calculator.py
+++ forex_calculator.py
@@ -1,11 +1,46 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox  # import messagebox for displaying warnings
+
+# List of Pairs
+intervals = ["USD", "CAD", "JPY", "NZD", "AUD"]
+
+pair1_value = "USD"
+pair2_value = "CAD"
+pair2_values = list(intervals)
+pair2_values.remove(pair1_value)
+
+# Risk percentage values
+risk_values = [f"{i}%" for i in range(1, 26)]
+
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 def position():
-    size = int(10)
-    return
+    position_size_str = position_size_TXT.get()
+    risk_str = risk_combobox.get().rstrip('%')
 
-def update_pair2(*args):
+    if not is_number(position_size_str):
+        messagebox.showwarning("Invalid input", "Please enter a valid numeric value for position size.")
+        return
+
+    position_size = float(position_size_str)
+    risk = float(risk_str) / 100
+    result = position_size * risk
+    result_label_TXT.delete(0, tk.END)
+    result_label_TXT.insert(0, str(result))
+
+def curr_pair1(*args):
+    pair1_values = list(intervals)
+    pair1_values.remove(pair2_value)
+    pair1_combobox['values'] = pair1_values
+    pair1_combobox.current(0)
+
+def curr_pair2(*args):
     # Get the selected value in Pair I combobox
     pair1_value = pair1_combobox.get()
 
@@ -31,8 +66,7 @@ window.rowconfigure(1, weight=1)
 window.rowconfigure(2, weight=1)
 window.rowconfigure(3, weight=1)
 
-# List of interval values
-intervals = ["USD", "CAD", "JPY", "NZD", "AUD"]
+
 
 # PANEL 0 -----------------------------------------------
 panel0 = tk.Frame(window)
@@ -45,12 +79,6 @@ pair1_combobox = ttk.Combobox(panel0, values=intervals)
 pair1_combobox.current(0)
 pair1_combobox.grid(row=1, column=0, sticky="ew")
 
-# Position size and label
-ticker_label = tk.Label(panel0, text="Position size:")
-ticker_label.grid(row=2, column=0, sticky="w")
-ticker_entry = tk.Entry(panel0)
-ticker_entry.grid(row=3, column=0, sticky="ew")
-
 # PANEL 1 -----------------------------------------------
 panel1 = tk.Frame(window)
 panel1.grid(row=0, column=2, rowspan=2, sticky="nsew")
@@ -58,30 +86,45 @@ panel1.grid(row=0, column=2, rowspan=2, sticky="nsew")
 # PANEL 1 - Combobox for Pair II
 pair2_label = tk.Label(panel1, text="Pair II:")
 pair2_label.grid(row=0, column=0, sticky="w")
-pair2_combobox = ttk.Combobox(panel1, values=intervals)
+pair2_combobox = ttk.Combobox(panel1, values=pair2_values)
 pair2_combobox.current(0)
 pair2_combobox.grid(row=1, column=0, sticky="ew")
 
-# Bind the Pair I combobox to the update_pair2 function
-pair1_combobox.bind("<<ComboboxSelected>>", update_pair2)
+# Bind the Pair I combobox to the curr_pair2 function
+pair1_combobox.bind("<<ComboboxSelected>>", curr_pair2)
 
 # NEW PANEL UNDER PANEL 0 AND PANEL 1 ----------------
 panel01 = tk.Frame(window)
 panel01.grid(row=2, column=0, columnspan=3, sticky="nsew")
 
-# Label for position size
+# POSITION SIZE
 position_size_label = tk.Label(panel01, text="Position Size:")
 position_size_label.grid(row=0, column=0, sticky="w")
+#
+position_size_TXT = tk.Entry(panel01)
+position_size_TXT.grid(row=0, column=1, sticky="ew")
 
-# Entry widget for position size
-position_size_entry = tk.Entry(panel01)
-position_size_entry.grid(row=0, column=1, sticky="ew")
+# RISK
+risk_label = tk.Label(panel01, text="Risk:")
+risk_label.grid(row=1, column=0, sticky="w")
+#
+risk_combobox = ttk.Combobox(panel01, values=risk_values)
+risk_combobox.current(0)
+risk_combobox.grid(row=1, column=1, sticky="ew")
 
-# PANEL 0 - Combobox for Calculate
-display_button = tk.Button(panel01, text="Calculate", command=position())
-display_button.grid(row=0, column=2, sticky="e", padx=5)
+# CALCULATE BUTTON
+display_button = tk.Button(panel01, text="Calculate", command=position)
+display_button.grid(row=2, column=1, sticky="e", padx=5)
+
+# RESULT
+result_label = tk.Label(panel01, text="Result:")
+result_label.grid(row=3, column=0, sticky="w")
+#
+result_label_TXT = tk.Entry(panel01)
+result_label_TXT.grid(row=3, column=1, sticky="ew")
 
 #--------------------------------------------------------
 # Start the Tkinter main loop
+
 window.mainloop()
 #--------------------------------------------------------
